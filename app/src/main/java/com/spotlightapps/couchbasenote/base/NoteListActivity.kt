d9375a1b08c8abb6421e59a6b74168224dca5c03
jawidMuhammadi <spotlightapps18@gmail.com>
2020-05-21 00:31:25 +0530

    class names refactored

--- app/src/main/java/com/spotlightapps/couchbasenote/base/NoteListActivity.kt
+++ app/src/main/java/com/spotlightapps/couchbasenote/base/NoteListActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.spotlightapps.couchbasenote.R
 
-class MainActivity : AppCompatActivity() {
+class NoteListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_note_list)
     }
 }
---
+++ app/src/main/java/com/spotlightapps/couchbasenote/login/LoginActivity.kt
@@ -0,0 +1,64 @@
+package com.spotlightapps.couchbasenote.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.snackbar.Snackbar
+import com.spotlightapps.couchbasenote.AppRepository
+import com.spotlightapps.couchbasenote.EventObserver
+import com.spotlightapps.couchbasenote.R
+import com.spotlightapps.couchbasenote.base.NoteListActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val factory = LoginViewModelFactory(AppRepository.getInstance(this))
+        viewModel = ViewModelProviders.of(this, factory).get(LoginViewModel::class.java)
+        subscribeUI()
+        btnLogin.setOnClickListener {
+            viewModel.onLoginButtonClicked()
+        }
+        tvSingUp.setOnClickListener {
+            viewModel.onSignUpClicked()
+        }
+    }
+
+
+    private fun subscribeUI() {
+        viewModel.onLoginClicked.observe(this, EventObserver {
+            val profile = viewModel.getUser(etName.text.toString())
+            profile?.let {
+                if (profile.password == etPassword.text.toString().trim()) {
+                    navigateToNoteListActivity()
+                } else {
+                    Snackbar.make(
+                        findViewById(R.id.login_base_layout),
+                        R.string.wrong_password,
+                        Snackbar.LENGTH_SHORT
+                    )
+                }
+            } ?: Toast.makeText(this, "User is not registered", Toast.LENGTH_SHORT).show()
+
+
+        })
+
+        viewModel.onSignUpClicked.observe(this, EventObserver {
+            viewModel.saveUser(etName.text.toString(), etPassword.text.toString())
+        })
+    }
+
+    private fun navigateToNoteListActivity() {
+        startActivity(Intent(this, NoteListActivity::class.java))
+    }
+
+
+}
